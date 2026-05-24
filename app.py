457d1ab23f244748d17a83d0ffa854af9d90551e
akinn <akin.nalruf@gmail.com>
2026-05-24 20:48:38 +0200

    dod

--- app.py
+++ app.py
@@ -872,4 +872,5 @@ def today_groups():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=False, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)

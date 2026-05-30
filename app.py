8cd599784a5c35519a78fa5277d186416f72a120
akinn <akin.nalruf@gmail.com>
2026-05-30 09:23:23 +0200

    run init_db on startup for gunicorn

--- app.py
+++ app.py
@@ -939,7 +939,8 @@ def today_groups():
     return jsonify({'today':date.today().strftime('%d.%m.%Y'),'day_name':DAYS_SL[date.today().weekday()],
         'groups':groups,'active_members':active_members,'sessions_this_week':sessions_this_week,'unpaid':unpaid})
 
+init_db()
+
 if __name__ == '__main__':
-    init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
